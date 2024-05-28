48b8802bfbffc7eb2f6f2289fe554e030b32a6fb
yeasinarafat51 <aloondokar65@gmail.com>
2024-05-29 00:44:16 +0600

    coursel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
             )}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">SHOWES</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -9,7 +9,7 @@ const Products = ({data}) => {
         <div>
             <h1 className="my-8 text-2xl font-bold text-center">Our Products</h1>
 
-            <div className="flex gap-4 justify-center items-center ">
+            <div className="flex flex-wrap gap-4 justify-center items-center ">
                {
                 data.slice(0,3)?.map((shoe) =>
                     (<SingleProduct key={shoe.id} shoe={shoe}/>)
