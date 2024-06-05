470814ac47ba2d17436b62f14edc4fb53a61bd81
yeasinarafat51 <aloondokar65@gmail.com>
2024-06-05 23:59:13 +0600

    text

--- src/pages/Registetion.jsx
+++ src/pages/Registetion.jsx
@@ -60,7 +60,7 @@ const Registetion = () => {
       <form onSubmit={handleSUbmit} className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Register now!</h1>
+            <h1 className="text-5xl font-bold text-cyan-500">Register now!</h1>
             <p className="py-6">
               {/* Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
