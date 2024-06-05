c6bfce8b58d89f8d44bff6e1e5d58236312c210b
yeasinarafat51 <aloondokar65@gmail.com>
2024-06-06 00:03:40 +0600

    log

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import GoogleLogin from "../components/GoogleLogin";
 import useAuth from "../Hooks/useAuth";
+import log from "../assets/log.jfif"
 
 const Login = () => {
   const {signIn,user} = useAuth()
@@ -24,11 +25,12 @@ const Login = () => {
     <form onSubmit={handleSUbmit} className="hero min-h-screen bg-base-200">
     <div className="hero-content flex-col lg:flex-row-reverse">
       <div className="text-center lg:text-left">
-        <h1 className="text-5xl font-bold">Login now!</h1>
+        <h1 className="text-5xl font-bold text-cyan-500">Login now!</h1>
         <p className="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          {/* Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
           excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-          a id nisi.
+          a id nisi. */}
+          <img src={log} alt="" />
         </p>
       </div>
       <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
